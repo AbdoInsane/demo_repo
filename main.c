c50e155114d3e082f4fd995921766a226e2cf970
Abdo <aitmohiabdlah@gmail.com>
2025-07-02 17:49:47 +0100

    Enter Name

--- main.c
+++ main.c
@@ -2,7 +2,9 @@
 
 int	main()
 {
-	printf("Hello World");
+	char name[30]; // characters array
+	scanf("%s", name);
+	printf("Hello %s", name); // prints 'Hello ' followed by the name
 
 	return 0;
 }
